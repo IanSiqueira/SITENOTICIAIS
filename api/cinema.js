63e470f32df72f7df0d1d3754a4e6f275ed38987
IanSiqueira <iancadori77@gmail.com>
2025-06-15 21:46:41 -0300

    Setimo commit do projeto

--- api/cinema.js
+++ api/cinema.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
   const { page = 1 } = req.query;
-
-  const url = `https://newsapi.org/v2/everything?q=cinema+filmes+brasil&language=pt&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=SEU_API_KEY_AQUI`;
+  const API_KEY = 'abdace74bc5b470bbae0c823922466e8';
+  const url = `https://newsapi.org/v2/everything?q=cinema+filmes+brasil&language=pt&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=${API_KEY}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar notícias de cinema' });
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao buscar notícias.' });
   }
 }
