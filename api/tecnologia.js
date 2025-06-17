8bb44b92e46b61fac012d1a3d32a51a2ed63d7b6
IanSiqueira <iancadori77@gmail.com>
2025-06-16 23:22:50 -0300

    47º Commit - corrigido política

--- api/tecnologia.js
+++ api/tecnologia.js
@@ -1,19 +1,18 @@
-// api/politica.js
-import express from 'express';
-import fetch from 'node-fetch';
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-  const page = req.query.page || 1;
-  const apiKey = process.env.NEWS_API_KEY;
+export default async function handler(req, res) {
+  const { page = 1 } = req.query;
+  const API_KEY = 'abdace74bc5b470bbae0c823922466e8';
+  const url = `https://newsapi.org/v2/everything?q=tecnologia+brasil&language=pt&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=${API_KEY}`;
 
   try {
-    const response = await fetch(`https://newsapi.org/v2/everything?q=política+brasil&language=pt&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=${apiKey}`);
+    const response = await fetch(url);
     const data = await response.json();
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Erro ao buscar notícias.' });
-  }
-});
 
-export default router;
+    if (data.status !== "ok") {
+      return res.status(500).json({ error: "Erro da NewsAPI", detalhes: data });
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao buscar notícias de tecnologia.' });
+  }
+}
